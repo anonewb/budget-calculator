9743e8164ab68704a4007acd3c739d6dea461583
ano <anonewbe@gmail.com>
2018-06-15 17:46:02 +0530

    ***Created CONSTRUCTORS and DATA STRUCTURES

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-  
+  //* CONSTRUCTORS
+  // we need to create expense object to store properties of the item(expense) like id, desc, value
+  // we created the constructor of 'Expense' obj as we want to create lots of objects(expense lists)  
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+  // now we can instantiate a new obj easily
+
+  //* DATA STRUCTURE
+  // Creating a neat data structure to store items and value
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
 
 })(); // IIFE
 
@@ -10,7 +37,6 @@ var budgetController = (function() {
 var UIController = (function() {
 
   var DOMSelectors = {
-
     inputType: '.add__type',
     inputDesc: '.add__description',
     inputValue: '.add__value',
@@ -30,11 +56,13 @@ var UIController = (function() {
     }
   }
 
-})();
+})(); // IIFE
 
 
 // APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) { 
+var controller = (function(budgetCtrl, UICtrl) { //* Used slightly different names 'budgetCtrl' coz if we have to change the controller name of 'budgetController' to something else, then we dont need to replace its name inside this App controller as 'budgetCtrl' is being used
+
+  // private methods: cannot be accessed by outside modules to avoid data override
 
   var LoadEventListeners = function() {
 
@@ -63,12 +91,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   } 
 
+  // public methods: can be accessed by outside modules
   return {
     init: function() {
       LoadEventListeners();
     }
   }
 
-})(budgetController, UIController);
+})(budgetController, UIController); // Can now access public methods present in budgetCtrl and UICtrl
 
+// Initializing our App
 controller.init();
